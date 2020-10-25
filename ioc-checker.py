b1891f8bbd54233dbba4577dbdb0192cf80677ea
J. R. Lambea <hello@jrlambea.me>
2020-10-25 18:17:14 +0100

    Change IP parser, add requirements and rename

--- ioc-checker.py
+++ ioc-checker.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 ##################################################################################################################################
 # Script to fetch the Domain and IP data from Risk IQ, VirusTotal API and to scrape the categorization from Symantec site review using selenium.
 # Author : Hem aka Cyberdude
@@ -8,32 +10,45 @@
 import json
 import requests
 import os
-import re
+import ipaddress
 from requests.auth import HTTPBasicAuth
 from argparse import ArgumentParser
 from selenium import webdriver
 from time import sleep
-from config import user, pwd, apikey    
+from config import user, pwd, apikey
+from selenium.webdriver.chrome.options import Options
+
+
+def parse_ip(ip):
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except:
+        return False
+
 
 def fetch_details(urls, user, pwd, entity):
-    #global declaration below so that these variables are available for code inside main()
+    # global declaration below so that these variables are available for code inside main()
     global classification, sinkhole, everCompromised, count_of_subdom, dynamicDns, mal_results, osint_results
     for ptUrl in urls:
         if ptUrl == ptEnrichUrl:
             payload = {'query': entity}
-            headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8', 'User-Agent': 'Mozilla/5.0'}
-            #In earlier requests version, params was replaced by data
+            headers = {'content-type': 'application/json',
+                       'Accept-Charset': 'UTF-8', 'User-Agent': 'Mozilla/5.0'}
+            # In earlier requests version, params was replaced by data
             resp = requests.get(ptUrl,
-                    params = payload,
-                    headers = headers,
-                    auth=HTTPBasicAuth(user, pwd))
+                                params=payload,
+                                headers=headers,
+                                auth=HTTPBasicAuth(user, pwd))
 
             jsonResp = resp.json()
-            #The original json values had non-json quotes which we are replacing with double quotes.
-            #Also, strings like True, False, None are causing problems for which we are replacing them using nested replace function
-            jsonResponse = str(jsonResp).replace('\'', '\"').replace('False','\"False\"').replace('None','[]').replace('True','\"True\"')
-            #print(jsonResponse)
-            #Converting json string objects to json dictionary
+            # The original json values had non-json quotes which we are replacing with double quotes.
+            # Also, strings like True, False, None are causing problems for which we are replacing them using nested replace function
+            jsonResponse = str(jsonResp).replace('\'', '\"').replace(
+                'False', '\"False\"').replace('None', '[]').replace('True', '\"True\"')
+
+            # print(jsonResponse)
+            # Converting json string objects to json dictionary
             try:
                 pt_dict = json.loads(jsonResponse)
                 classification = pt_dict['classification']
@@ -43,25 +58,27 @@ def fetch_details(urls, user, pwd, entity):
                 dynamicDns = pt_dict['dynamicDns']
             except:
                 #print('Error in parsing Enrichment json data for:', entity)
-                count_of_subdom="JSON_Error"
-                classification="JSON_Error"
-                dynamicDns="JSON_Error"
+                count_of_subdom = "JSON_Error"
+                classification = "JSON_Error"
+                dynamicDns = "JSON_Error"
                 continue
         elif ptUrl == ptMalwareUrl:
             payload = {'query': entity}
-            headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8', 'User-Agent': 'Mozilla/5.0'}
-            #In earlier requests version, params was replaced by data
+            headers = {'content-type': 'application/json',
+                       'Accept-Charset': 'UTF-8', 'User-Agent': 'Mozilla/5.0'}
+            # In earlier requests version, params was replaced by data
             resp = requests.get(ptUrl,
-                    params = payload,
-                    headers = headers,
-                    auth=HTTPBasicAuth(user, pwd))
+                                params=payload,
+                                headers=headers,
+                                auth=HTTPBasicAuth(user, pwd))
 
             jsonResp = resp.json()
-            #The original json values had non-json quotes which we are replacing with double quotes.
-            #Also, strings like True, False, None are causing problems for which we are replacing them using nested replace function
-            jsonResponse = str(jsonResp).replace('\'', '\"').replace('False','\"False\"').replace('None','[]').replace('True','\"True\"')
-            #print(jsonResponse)
-            #Converting json string objects to json dictionary
+            # The original json values had non-json quotes which we are replacing with double quotes.
+            # Also, strings like True, False, None are causing problems for which we are replacing them using nested replace function
+            jsonResponse = str(jsonResp).replace('\'', '\"').replace(
+                'False', '\"False\"').replace('None', '[]').replace('True', '\"True\"')
+            # print(jsonResponse)
+            # Converting json string objects to json dictionary
             try:
                 pt_dict = json.loads(jsonResponse)
                 mal_results = len(pt_dict['results'])
@@ -71,19 +88,21 @@ def fetch_details(urls, user, pwd, entity):
                 continue
         else:
             payload = {'query': entity}
-            headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8', 'User-Agent': 'Mozilla/5.0'}
-            #In earlier requests version, params was replaced by data
+            headers = {'content-type': 'application/json',
+                       'Accept-Charset': 'UTF-8', 'User-Agent': 'Mozilla/5.0'}
+            # In earlier requests version, params was replaced by data
             resp = requests.get(ptUrl,
-                    params = payload,
-                    headers = headers,
-                    auth=HTTPBasicAuth(user, pwd))
+                                params=payload,
+                                headers=headers,
+                                auth=HTTPBasicAuth(user, pwd))
 
             jsonResp = resp.json()
-            #The original json values had non-json quotes which we are replacing with double quotes.
-            #Also, strings like True, False, None are causing problems for which we are replacing them using nested replace function
-            jsonResponse = str(jsonResp).replace('\'', '\"').replace('False','\"False\"').replace('None','[]').replace('True','\"True\"')
-            #print(jsonResponse)
-            #Converting json string objects to json dictionary
+            # The original json values had non-json quotes which we are replacing with double quotes.
+            # Also, strings like True, False, None are causing problems for which we are replacing them using nested replace function
+            jsonResponse = str(jsonResp).replace('\'', '\"').replace(
+                'False', '\"False\"').replace('None', '[]').replace('True', '\"True\"')
+            # print(jsonResponse)
+            # Converting json string objects to json dictionary
             try:
                 pt_dict = json.loads(jsonResponse)
                 osint_results = len(pt_dict['results'])
@@ -93,22 +112,28 @@ def fetch_details(urls, user, pwd, entity):
                 continue
     return classification, sinkhole, everCompromised, count_of_subdom, dynamicDns, mal_results, osint_results
 
+
 class siteReview():
     def __init__(self):
-        #To instantiate the chrome browser
-        self.driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+
+        # To instantiate the chrome browser
+        self.driver = webdriver.Chrome(options=chrome_options)
 
     def ioc_search(self, entity):
         # self.driver.get('https://sitereview.bluecoat.com/#/')
         url = 'https://sitereview.bluecoat.com/#/lookup-result/' + entity
         self.driver.get(url)
-        #With out this sleep function, the site review rejects the requests. This will help to throttle the requests.
+        # With out this sleep function, the site review rejects the requests. This will help to throttle the requests.
         sleep(6)
 
         try:
-            cat = self.driver.find_element_by_xpath('//*[@id="submissionForm"]/span/span[1]/div/div[2]/span[1]/span')
+            cat = self.driver.find_element_by_xpath(
+                '//*[@id="submissionForm"]/span/span[1]/div/div[2]/span[1]/span')
             if self.driver.find_element_by_xpath('//*[@id="submissionForm"]/span/span[1]/div/div[2]/span[2]/span'):
-                cat2 = self.driver.find_element_by_xpath('//*[@id="submissionForm"]/span/span[1]/div/div[2]/span[2]/span')
+                cat2 = self.driver.find_element_by_xpath(
+                    '//*[@id="submissionForm"]/span/span[1]/div/div[2]/span[2]/span')
                 if "Last Time" in cat2.text:
                     category = cat.text
                 else:
@@ -131,15 +156,13 @@ def lst_parse(lst, urls, user, pwd, apikey):
             val = bot.ioc_search(entity)
 
             # VT Processing
-            ip_regex = '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(
-                        25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(
-                        25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(
-                        25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$'''
-            if (re.search(ip_regex, entity)):
+
+            if (parse_ip(entity)):
                 url = 'https://www.virustotal.com/api/v3/ip_addresses/' + entity
                 headers = {'x-apikey': apikey}
                 response = requests.get(url, headers=headers)
                 vtResp = response.json()
+
                 if response.status_code == 200:
                     # print(vtResp)
                     # We are using string replacement below to adjust the JSON response to be loaded into python dictionary
@@ -172,13 +195,8 @@ def lst_parse(lst, urls, user, pwd, apikey):
                     reputation = 'VT Error'
                     tags = 'VT Error'
 
-                cats = 'AS Owner' + '-' + str(as_own).replace(',', '') + '|' + 'ASN' + '-' + str(
-                    asn) + '|' + 'Stats' + '-' + str(stats).replace(', ', ';').replace('{', '').replace('}',
-                                                                                                        '').replace(
-                    '\'', '') + '|' + 'Network' + '-' + str(network) + '|' + 'Reputation Score' + '-' + str(
-                    reputation) + '|' + 'Tags' + '-' + str(tags).replace('[', '').replace(']', '')
-                print(entity, classification, sinkhole, everCompromised, count_of_subdom, dynamicDns, mal_results,
-                      osint_results, cats, val, sep=",")
+                cats = 'AS Owner' + '-' + str(as_own).replace(',', '') + '|' + 'ASN' + '-' + str(asn) + '|' + 'Stats' + '-' + str(stats).replace(', ', ';').replace('{', '').replace('}', '').replace(
+                    '\'', '') + '|' + 'Network' + '-' + str(network) + '|' + 'Reputation Score' + '-' + str(reputation) + '|' + 'Tags' + '-' + str(tags).replace('[', '').replace(']', '')
 
             else:
                 url = 'https://www.virustotal.com/api/v3/domains/' + entity
@@ -197,8 +215,10 @@ def lst_parse(lst, urls, user, pwd, apikey):
                         cats = attr['categories']
                         stats = attr['last_analysis_stats']
                         # print(cats)
-                        cats = str(cats).replace(', ', '|').replace('{', '').replace('}', '').replace('\'', '')
-                        stats = str(stats).replace(', ', ';').replace('{', '').replace('}','').replace('\'', '')
+                        cats = str(cats).replace(', ', '|').replace(
+                            '{', '').replace('}', '').replace('\'', '')
+                        stats = str(stats).replace(', ', ';').replace(
+                            '{', '').replace('}', '').replace('\'', '')
                     except:
                         cats = 'null'
                         stats = 'null'
@@ -218,11 +238,8 @@ def cmd_parse(cmd, urls, user, pwd, apikey):
     val = bot.ioc_search(entity)
 
     # VT Processing
-    ip_regex = '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(
-                          25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(
-                          25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(
-                          25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$'''
-    if (re.search(ip_regex, entity)):
+
+    if (parse_ip(entity)):
         url = 'https://www.virustotal.com/api/v3/ip_addresses/' + entity
         headers = {'x-apikey': apikey}
         response = requests.get(url, headers=headers)
@@ -260,12 +277,12 @@ def cmd_parse(cmd, urls, user, pwd, apikey):
             tags = 'VT Error'
 
         cats = 'AS Owner' + '-' + str(as_own).replace(',', '') + '|' + 'ASN' + '-' + str(
-          asn) + '|' + 'Stats' + '-' + str(stats).replace(', ', ';').replace('{', '').replace('}',
-                                                                                              '').replace(
-          '\'', '') + '|' + 'Network' + '-' + str(network) + '|' + 'Reputation Score' + '-' + str(
-          reputation) + '|' + 'Tags' + '-' + str(tags).replace('[', '').replace(']', '')
+            asn) + '|' + 'Stats' + '-' + str(stats).replace(', ', ';').replace('{', '').replace('}',
+                                                                                                '').replace(
+            '\'', '') + '|' + 'Network' + '-' + str(network) + '|' + 'Reputation Score' + '-' + str(
+            reputation) + '|' + 'Tags' + '-' + str(tags).replace('[', '').replace(']', '')
         print(entity, classification, sinkhole, everCompromised, count_of_subdom, dynamicDns, mal_results,
-            osint_results, cats, val, sep=",")
+              osint_results, cats, val, sep=",")
 
     else:
         url = 'https://www.virustotal.com/api/v3/domains/' + entity
@@ -283,18 +300,32 @@ def cmd_parse(cmd, urls, user, pwd, apikey):
                 attr = vt_dict['attributes']
                 cats = attr['categories']
                 # print(cats)
-                cats = str(cats).replace(', ', '|').replace('{', '').replace('}', '').replace('\'', '')
+                cats = str(cats).replace(', ', '|').replace(
+                    '{', '').replace('}', '').replace('\'', '')
             except:
                 cats = 'null'
         else:
             cats = 'VT Error'
 
-        print(entity, classification, sinkhole, everCompromised, count_of_subdom, dynamicDns, mal_results,
-            osint_results, cats, val, sep=",")
+        test = {
+            "ioc": entity,
+            "classification": classification,
+            "sinkhole": sinkhole,
+            "ever_compromised": everCompromised,
+            "count_of_subdom": count_of_subdom,
+            "dynamic_dns": dynamicDns,
+            "mal_results": mal_results,
+            "osint_results": osint_results,
+            "VT_Data": cats,
+            "Symantec_Sitereview": val
+        }
+
+        print(test)
+        # print(entity, classification, sinkhole, everCompromised, count_of_subdom, dynamicDns, mal_results, osint_results, cats, val, sep=",")
 
 
 def main():
-    #Creating a list of URLs
+    # Creating a list of URLs
     global ptEnrichUrl, ptMalwareUrl, ptOsintUrl
     ################### Passive Total URLs ########################
     ptEnrichUrl = "https://api.passivetotal.org/v2/enrichment"
@@ -305,9 +336,11 @@ def main():
     urls = [ptEnrichUrl, ptMalwareUrl, ptOsintUrl]
 
     p = ArgumentParser()
-    p.add_argument("-l", "--lst", type=str, help="Submit domain/IP list separated by new line specifying the absolute path of file")
+    p.add_argument("-l", "--lst", type=str,
+                   help="Submit domain/IP list separated by new line specifying the absolute path of file")
     p.add_argument("-c", "--cmd", type=str, help="Enter the single domain/IP")
     args = p.parse_args()
+
     if args.lst:
         print('ioc,classification,sinkhole,everCompromised,subdomains,dynamicDns,mal_results,osint_results,VT_Data,Symantec_Sitereview')
         lst_parse(args.lst, urls, user, pwd, apikey)
